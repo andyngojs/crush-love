4fb75ee5aa8d20250054f6936779b635c9260800
andyngojs <dongngo.2000@gmail.com>
2021-05-14 15:02:38 +0700

    add script js

---
+++ main.js
@@ -0,0 +1,34 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+var btnYes = $('.btn--yes')
+var btnNo = $('.btn--no')
+
+var popup = $('.modal')
+var overlay = $('.modal__overlay')
+var btnClose = $('.btn-close')
+
+window.onload = () => {
+    alert('Xin Chào ! Cậu đã sẵn sàng chưa ?')
+}
+
+btnYes.onclick = () => {
+    popup.classList.add('show')
+}
+
+btnClose.onclick = () => {
+    popup.classList.remove('show')
+}
+
+overlay.onclick = () => {
+    popup.classList.remove('show')
+}
+
+btnNo.onmouseover = (e) => {
+    console.log(e)
+    var x = Math.floor(Math.random() * e.relatedTarget.clientHeight)
+    var y = Math.floor(Math.random() * e.relatedTarget.clientWidth)
+
+    btnNo.style.top = x + "px"
+    btnNo.style.left = y + "px"
+}
