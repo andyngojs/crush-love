923cd2f56bf8280b91dd56a6f5915bbefc3a2385
andyngojs <dongngo.2000@gmail.com>
2021-05-14 15:41:17 +0700

    fix: fix js

--- main.js
+++ main.js
@@ -1,40 +1,43 @@
-const $ = document.querySelector.bind(document)
+function start() {
+    const $ = document.querySelector.bind(document)
 
-var btnYes = $('.btn--yes')
-var btnNo = $('.btn--no')
-var popup = $('.modal')
-var overlay = $('.modal__overlay')
-var btnClose = $('.btn-close')
+    var btnYes = $('.btn--yes')
+    var btnNo = $('.btn--no')
+    var popup = $('.modal')
+    var overlay = $('.modal__overlay')
+    var btnClose = $('.btn-close')
 
-window.onload = () => {
-    $('.music').play()
-}
-
-window.onkeypress = (e) => {
-    if (e.charCode === 32) {
+    window.onload = () => {
         $('.music').play()
+    }
+
+    window.onkeypress = (e) => {
+        if (e.charCode === 32) {
+            $('.music').play()
+        }
+    }
+
+    btnYes.onclick = () => {
+        popup.classList.add('show')
+    }
+
+    btnClose.onclick = () => {
+        popup.classList.remove('show')
+    }
+
+    overlay.onclick = () => {
+        popup.classList.remove('show')
+    }
+
+    btnNo.onmouseover = (e) => {
+        // console.log(e)
+        var x = Math.floor(Math.random() * e.relatedTarget.clientHeight)
+        var y = Math.floor(Math.random() * e.relatedTarget.clientWidth)
+
+        btnNo.style.top = x + "px"
+        btnNo.style.left = y + "px"
     }
 }
 
-btnYes.onclick = () => {
-    popup.classList.add('show')
-}
 
-btnClose.onclick = () => {
-    popup.classList.remove('show')
-}
-
-overlay.onclick = () => {
-    popup.classList.remove('show')
-}
-
-btnNo.onmouseover = (e) => {
-    // console.log(e)
-    var x = Math.floor(Math.random() * e.relatedTarget.clientHeight)
-    var y = Math.floor(Math.random() * e.relatedTarget.clientWidth)
-
-    btnNo.style.top = x + "px"
-    btnNo.style.left = y + "px"
-}
-
-
+start()
