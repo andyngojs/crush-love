7cd3d4e92c10ade8bd2f483b90fe9c33d8068c7d
andyngojs <dongngo.2000@gmail.com>
2021-07-03 14:03:41 +0700

    update 3-7-21

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,61 +1,94 @@
-$(document).ready(function() {
-    setTimeout(function() {
-        $('.spinner').fadeOut()
-        $('#preloader').delay(350).fadeOut('slow')
-        $('body').delay(350).css({
-            'overflow': 'visible'
-        })
-    }, 600)
-})
+// CONFIG DATA 
+const CONFIGDATA = {
+    titleHeader: 'Làm người yêu tớ nhé I Love You',
+    descriptionHeader: 'Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^',
+    buttonYes: 'Đồng Ý',
+    buttonNo: 'Không! Còn lâu nhé.',
+    titleModar: 'Cảm ơn em vì đồng ý',
+    descriptionModar: 'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+};
 
-function start() {
-    var btnYes = document.querySelector('.btn--yes')
-    var btnNo = document.querySelector('.btn--no')
-    var btnOpen = document.querySelector('.btn--open')
-    var popup = document.querySelector('.modal')
-    var overlay = document.querySelector('.modal__overlay')
-    var btnClose = document.querySelector('.btn-close')
+
+
+
+
+// Author: andyngojs
+// Facebook: http://fb.com/dn279
+// Join us: https://fb.com/groups/dobeeteam.community
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Không được thay đổi ở đây - Not to change
+$(document).ready(function() {setTimeout(function() {$('.spinner').fadeOut();$('#preloader').delay(350).fadeOut('slow');$('body').delay(350).css({'overflow': 'visible'})}, 600)});
+
+function start(data) {
+    var btnYes = document.querySelector('.btn--yes');
+    var btnNo = document.querySelector('.btn--no');
+    var popup = document.querySelector('.modal');
+    var overlay = document.querySelector('.modal__overlay');
+    var btnClose = document.querySelector('.btn-close');
+    var headerModar = document.querySelector('.heading-name');
+    var desccriptionModar = document.querySelector('.message');
 
     window.onload = () => {
         document.querySelector('.music').play()
         document.querySelector('.music').volume = 1
-    }
-
+    };
     window.onkeypress = (e) => {
         if (e.charCode === 32) {
-            document.querySelector('.music').play()
+            document.querySelector('.music').play();
         }
-    }
+    };
 
     document.querySelector('.header').innerHTML = `
         <h1 class="header-name">
-            Làm Người Yêu Tớ Nhé ? ^^ I Love You
+            ${data.titleHeader}
             <i class="ti-heart"></i>
         </h1>
-        <p class="header-description">Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^
+        <p class="header-description">${data.descriptionHeader}
             <i class="ti-face-smile"></i>
-        </p>`
-
+        </p>`;
+    btnYes.innerHTML = `<i class="ti-thumb-up"></i> ${data.buttonYes}`;
+    btnNo.innerHTML =  ` <i class="ti-thumb-down"></i> ${data.buttonNo}`;
+    headerModar.innerHTML = `${data.titleModar} <i class="ti-heart"></i>`;
+    desccriptionModar.innerHTML = `${data.descriptionModar}`
 
     btnYes.onclick = () => {
-        popup.classList.add('show')
-    }
-
+        popup.classList.add('show');
+    };
     btnClose.onclick = () => {
-        popup.classList.remove('show')
-    }
+        popup.classList.remove('show');
+    };
 
     overlay.onclick = () => {
-        popup.classList.remove('show')
-    }
+        popup.classList.remove('show');
+    };
+
+    btnNo.onclick = () => {
+        btnNo.style.top = 100 + "px";
+        btnYes.style.top = 280 + "px";
+
+    };
 
     btnNo.onmouseover = (e) => {
-        var x = Math.random() * e.relatedTarget.clientHeight * 0.9
-        var y = Math.random() * e.relatedTarget.clientWidth * 0.9
-        btnNo.style.top = x + "px"
-        btnNo.style.right = y + "px"
-    }
+        var x = Math.random() * e.relatedTarget.clientHeight * 0.9;
+        var y = Math.random() * e.relatedTarget.clientWidth * 0.9;
+        btnNo.style.top = x + "px";
+        btnNo.style.right = y + "px";
+    };
 
-}
-start()
+};
+
+start(CONFIGDATA);
 
---
+++ readme.md
@@ -0,0 +1,31 @@
+# Trang web tỏ tình crush cực kỳ lãng mạn 
+
+Link Demo: https://j2c.cc/LOVE-CRUSH
+
+## Author
+
+*Facebook*: <a href="http://fb.com/dn279">andyngojs</a>
+
+## Join Us
+
+*Group Facebook*:  <a href="https://fb.com/groups/dobeeteam.community">DOBEE Community</a>
+
+## Cách sử dụng: 
+
+Nếu bạn muốn dùng web này theo cách của bạn hoặc muốn đưa lên host riêng của bạn để gửi cho người thầm thương nhớ của bạn 
+
+thì hãy đọc hướng dẫn sử dụng này.
+
+**Note**: Nếu các bạn không thích động tay động chân thì dùng luôn link demo ở phía trên.
+
+Ở đây chúng ta dùng github page (nếu các bạn có host riêng thì đưa source code này lên host của bạn).
+
+Bước 1: Click vào `Star` và `Fork` để clone repo này về github của bạn.
+
+Bước 2: Vào trang cá nhân github của bạn và chuyển sang tab Repositories. Sẽ nhìn thấy repo mà bạn vừa Fork về và click vào repo đó
+
+Bước 3: Chuyển sang tab Setting và lướt xuống dưới thấy chữ Github Page thì click vào `check it out here` và chọn nhánh Main rồi click Save.
+
+Ngồi chờ 5 phút đợi web của bạn tải lên sau đó sẽ hiện link trang web bạn click vào link đó và tận hưởng thành quả gửi cho crush thôi.
+
+## Video Hướng dẫn
