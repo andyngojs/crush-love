43aeb37382342cef1cbee0e0b378f7dbbe5e9521
pritanium <88266495+pritanium@users.noreply.github.com>
2021-08-13 09:29:04 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 // CONFIG DATA 
 const CONFIGDATA = {
-    titleHeader: 'Làm người yêu tớ nhé I Love You',
+    titleHeader: 'Làm người yêu tớ nha ',
     descriptionHeader: 'Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^',
     buttonYes: 'Đồng Ý',
     buttonNo: 'Không! Còn lâu nhé.',
     titleModar: 'Cảm ơn em vì đồng ý',
-    descriptionModar: 'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu.Đối với tớ, cậu mãi là cô bé :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
+    descriptionModar: 'Tớ biết là cậu sẽ đồng ý làm người yêu tớ mà ^^ </br> Cám ơn cậu đã đồng ý để cho tớ được che chở và bảo vệ cậu :)) </br> "Thích cậu nhiều đến mấy, cậu sẽ hiểu thôi. </br>  Tớ thích cậu nhiều đến mấy, cậu sẽ hiểu thôi "',
 };
 
 
