223bc57c0a3ff6ce0b8e3d78ac4c4026506018da
Datngo0506 <91180042+Datngo0506@users.noreply.github.com>
2021-09-22 14:49:56 +0700

    Update CONFIG.js

--- CONFIG.js
+++ CONFIG.js
@@ -1,5 +1,5 @@
 const CONFIGDATA = {
-  titleHeader: "Làm người yêu tớ nhé I Love You",
+  titleHeader: "Làm người yêu Thanh Tâm nhé I Love You",
   descriptionHeader:
     "Nếu cậu thoát ra và không trả lời thì có nghĩa là cậu đã ĐỒNG Ý rồi đấy ^^",
   buttonYes: "Đồng Ý",
